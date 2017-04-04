e5b1693943def85e6c419ba3d042550492291326
mandyonze <borjalor@ucm.es>
2017-04-04 19:44:47 +0200

    Login for upload

--- P2-Grupo8-Pachanga/js/login.js
+++ P2-Grupo8-Pachanga/js/login.js
@@ -2,24 +2,52 @@ function checkUser(){
 	var x = document.getElementById("username");
 
 	if (x.value.length < 5) {
-		document.getElementById("usernameError").innerHTML = "Debes introducir tu nombre de USUARIO correcto";
+		document.getElementById("usernameError").src = "img/iconos/error.png";
+		x.classList.remove('form-control-ok');
 		x.classList.add('form-control-error')
 	} else {
-		document.getElementById("usernameError").innerHTML = "";
-		x.classList.remove('form-control-error')
+
+		document.getElementById("usernameError").src = "img/iconos/ok.png";
+		x.classList.remove('form-control-error');
+		x.classList.add('form-control-ok');
 	}
 
 }
 
-function checkPassword(){
-	var x = document.getElementById("pasword");
+
+
+function checkPass(){
+	var x = document.getElementById("pass");
 
 	if (x.value.length < 5) {
-		document.getElementById("passwordError").innerHTML = "Debes introducir tu nombre de USUARIO correcto";
+		document.getElementById("passError").src = "img/iconos/error.png";
+
+		x.classList.remove('form-control-ok');
 		x.classList.add('form-control-error')
 	} else {
-		document.getElementById("passwordError").innerHTML = "";
-		x.classList.remove('form-control-error')
+
+		document.getElementById("passError").src = "img/iconos/ok.png";
+		x.classList.remove('form-control-error');
+		x.classList.add('form-control-ok');
 	}
 
 }
+
+
+
+
+
+
+
+// function checkPassword(){
+// 	var x = document.getElementById("pasword");
+//
+// 	if (x.value.length < 5) {
+// 		document.getElementById("passwordError").innerHTML = "Debes introducir tu nombre de USUARIO correcto";
+// 		x.classList.add('form-control-error')
+// 	} else {
+// 		document.getElementById("passwordError").innerHTML = "";
+// 		x.classList.remove('form-control-error')
+// 	}
+//
+// }
