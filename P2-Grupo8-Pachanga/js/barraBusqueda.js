0f1264f31c2f422b7b3ee954806d929eceabe42d
Andrés Herranz <andresherranz999@gmail.com>
2017-04-17 20:35:59 +0200

    barraBusqueda terminada

--- P2-Grupo8-Pachanga/js/barraBusqueda.js
+++ P2-Grupo8-Pachanga/js/barraBusqueda.js
@@ -5,3 +5,14 @@
       object.value = object.value.slice(0, object.maxLength)
   }
 
+
+$( "#input-gr" ).load(function() {
+  $('.input-group').css("float", "");
+});
+
+function removeFloat(object)
+{
+	alert("Hello! I am an alert box!");
+	$('.input-group').css("float", "");
+	object.css('float', '')
+}
