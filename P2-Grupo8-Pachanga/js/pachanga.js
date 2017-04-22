7c57e1dc0b8e8801cf4e57f38279b215bc25d854
Andrés Herranz <andresherranz999@gmail.com>
2017-04-22 18:30:34 +0200

    Compartir en proceso 1 (pachanga.js modificado)

--- P2-Grupo8-Pachanga/js/pachanga.js
+++ P2-Grupo8-Pachanga/js/pachanga.js
@@ -35,12 +35,34 @@ $(document).ready(function(){
 /***************************************************************************************************************************
   Change the visibility of formulario according to the action.
 ****************************************************************************************************************************/
+// function changeFormulario(id){
+
+//   if (id == "login") {
+//     document.getElementById("signUp").style.display = "none";
+//   } else {
+//     document.getElementById("login").style.display = "none";
+//   }
+
+//   if (document.getElementById(id).style.display == "block") {
+//     document.getElementById(id).style.display = "none";
+//   }else{
+//     document.getElementById(id).style.display = "block"
+//     cerrar(id);
+//   }
+
+// }
+
 function changeFormulario(id){
 
   if (id == "login") {
     document.getElementById("signUp").style.display = "none";
+    document.getElementById("compartir").style.display = "none";
+  } else if(id == "signUp") {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("compartir").style.display = "none";
   } else {
     document.getElementById("login").style.display = "none";
+    document.getElementById("signUp").style.display = "none";
   }
 
   if (document.getElementById(id).style.display == "block") {
@@ -49,8 +71,8 @@ function changeFormulario(id){
     document.getElementById(id).style.display = "block"
     cerrar(id);
   }
-
 }
+
 /***************************************************************************************************************************
   When the user clicks anywhere outside of the login/register, close it
 ****************************************************************************************************************************/
